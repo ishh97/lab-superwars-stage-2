1a37a244ea592a19fd706a73cc9d597f5d786159
ishh97 <142491547+ishh97@users.noreply.github.com>
2023-10-08 01:36:40 +0530

    canges in app.js

--- src/app.js
+++ src/app.js
@@ -23,10 +23,19 @@ const PLAYERS = [
 
 // initialize players with image and strength
 const initPlayers = (players) => {
-    let detailedPlayers = [];
+    // let detailedPlayers = [];
     // Create players using for loop
     // Type your code here
-
+    const detailedPlayers = players.map((player, index) => {
+        return {
+          name: player,
+          strength: 2 + index,
+          image: `images/super-${index + 1}.png`,
+          type: index % 2 === 0 ? "hero" : "villain",
+          id: index + 1,
+        };
+      });
+    
     return detailedPlayers;
 }
 
@@ -34,23 +43,57 @@ const initPlayers = (players) => {
 const getRandomStrength = () => {
     // Return a random integer (0,100]
     // Note: You can use Math.random() and Math.ceil()
+    return Math.ceil(Math.random() * 100);
 }
-
+// const view = (playerObj) => {
+//     let player = document.createElement('div');
+//     player.classList.add('player');
+//     let image = document.createElement('img');
+//     image.setAttribute('src', playerObj.image);
+//     image.setAttribute('alt', '');
+//     let name = document.createElement('div');
+//     name.className = 'name';
+//     name.textContent = playerObj.name;
+//     let strength = document.createElement('div');
+//     strength.textContent = playerObj.strength;
+//     strength.className = 'strength';
+//     player.append(image, name, strength);
+//     return player;
+//   };
+const view = (playerObj) => {
+    const { image, name, strength } = playerObj;
+  
+    const player = document.createElement('div');
+    player.classList.add('player');
+  
+    player.innerHTML = `
+      <img src="${image}" alt="">
+      <div class="name">${name}</div>
+      <div class="strength">${strength}</div>
+    `;
+  
+    return player;
+  };
+  
 const buildPlayers = (players, type) => {
-    let fragment = '';
+    let fragment = document.createElement('div');
+  players
+    .filter((player) => player.type == type)
+    .forEach((player) => fragment.append(view(player)));
+  return fragment.innerHTML;
 
     // Loop through players and accumulate HTML template
     // depending of type of player(hero|villain)
     // Type your code here
 
-    return fragment;
 }
 // Display players in HTML
 const viewPlayers = (players) => {
 
     document.getElementById('heroes').innerHTML = buildPlayers(players, 'hero');
     document.getElementById('villains').innerHTML = buildPlayers(players, 'villain');
-
+    buildPlayers(players, 'hero');
+    buildPlayers(players,'villain');
 }
 
 window.onload = () => {
